94f1be788e75cdac0e93f66944a9c05424e6f4b9
Pavel Smirnov <skqret@gmail.com>
2021-11-12 05:31:13 +0300

    Meow

---
+++ Js/Java.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('#input')
+const btn = document.querySelector('#btn')
+const result = document.querySelector('#result')
+
+
+//add event
+
+btn.addEventListener('click', (e) => {
+    console.log(input.value)
+})
